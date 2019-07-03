d90ca6b47b2918810d0fe82fa4081af3869f7e01
李楚军 <lcj0823>
2019-07-03 18:37:52 +0800

    注释修改

--- demo/src/main/java/com/lee/rokhan/demo/DemoServer.java
+++ demo/src/main/java/com/lee/rokhan/demo/DemoServer.java
@@ -1,20 +1,7 @@
 package com.lee.rokhan.demo;
 
-import com.lee.rokhan.container.advice.MethodBeforeAdvice;
-import com.lee.rokhan.container.advice.MethodReturnAdvice;
-import com.lee.rokhan.container.advice.MethodSurroundAdvice;
-import com.lee.rokhan.container.advisor.Advisor;
-import com.lee.rokhan.container.advisor.impl.AspectJPointcutAdvisor;
 import com.lee.rokhan.container.context.impl.AnnotationApplicationContext;
-import com.lee.rokhan.container.definition.impl.IocBeanDefinition;
-import com.lee.rokhan.container.factory.BeanFactory;
-import com.lee.rokhan.container.factory.impl.IocBeanFactory;
-import com.lee.rokhan.container.processor.impl.AdvisorAutoProxyCreator;
 import com.lee.rokhan.demo.controller.DemoController;
-import com.lee.rokhan.demo.service.impl.DemoServiceImpl;
-
-import java.lang.reflect.Method;
-import java.util.Collections;
 
 /**
  * @author lichujun
@@ -30,56 +17,6 @@ public class DemoServer {
         DemoController demoController = (DemoController) context.getBean("demoController");
         demoController.test();
 
-        /*DemoServiceImpl demoService = (DemoServiceImpl) context.getBean("demoServiceImpl");
-        demoService.test1();*/
-
-
-        /*IocBeanDefinition iocBeanDefinition = new IocBeanDefinition();
-        iocBeanDefinition.setBeanClass(DemoServer.class);
-
-        BeanFactory beanFactory = new IocBeanFactory();
-
-        // 注册切面
-        Advisor advisor = new AspectJPointcutAdvisor("demoAdvice", "execution(* com.lee.rokhan.demo.*.doSomething (..))");
-
-        IocBeanDefinition adviceBeanDefinition = new IocBeanDefinition();
-        adviceBeanDefinition.setBeanClass(DemoServer.class);
-        adviceBeanDefinition.setFactoryMethodName("createReturn");
-        beanFactory.registerBeanDefinition("demoAdvice", adviceBeanDefinition);
-
-
-        beanFactory.registerBeanPostProcessor(new AdvisorAutoProxyCreator(Collections.singletonList(advisor), beanFactory));
-        beanFactory.registerBeanDefinition("demo", iocBeanDefinition);
-        DemoServer demoServer = (DemoServer) beanFactory.getBean("demo");
-        demoServer.doSomething();*/
-
-
     }
 
-    public void doSomething() {
-        System.out.println("hello world");
-    }
-
-    public void doSome() {
-        System.out.println("hello world");
-    }
-
-    public static MethodSurroundAdvice createSurround() {
-        return (method, args, target) -> {
-            System.out.println("你是谁");
-            Object returnVal = method.invoke(target, args);
-            System.out.println("老子你都不认识了吗，吃屎去");
-            return returnVal;
-        };
-    }
-
-    public static MethodBeforeAdvice createBefore() {
-        return ((method, args, target) -> System.out.println("before"));
-    }
-
-    public static MethodReturnAdvice createReturn() {
-        return ((returnValue, method, args, target) -> {
-            System.out.println("return");
-        });
-    }
 }
